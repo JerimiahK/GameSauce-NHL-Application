07396b0116f952c90545174013f034abab2b9f1b
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-15 18:52:46 -0700

    set up the PageContainer with the NavTabs and Homepage to get a display working

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,43 +1,24 @@
 export default function NavTabs() {
-    <nav class="navbar">
-      <div class="container-fluid">
-        <a id="navbarText" href="/">
-          The NHL Project
-        </a>
-        <button
-          id="navbarBtn"
-          class="navbar-toggler ms-auto"
-          type="button"
-          data-mdb-toggle="collapse"
-          data-mdb-target="#navbarToggleExternalContent3"
-          aria-controls="navbarToggleExternalContent3"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapseBackground shadow-3 p-4">
-            <button class="btn btn-link btn-block border-bottom m-0">
-                <a class="dropdown-item" href="/">Home</a>
-            </button>
-            <button class="btn btn-link btn-block border-bottom m-0">
-                <a class="dropdown-item" href="/pages/current/games">Games</a>
-            </button>
-            <button class="btn btn-link btn-block m-0">
-                <a id="logoutBtn" class="dropdown-item" href="/">Sign Out</a>
-            </button>
+    return (
+      <nav class="navbar">
+        <div class="container-fluid">
+          <a id="navbarText" href="/">
+            The NHL Project
+          </a>
+          <button
+            id="navbarBtn"
+            class="navbar-toggler ms-auto"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarToggleExternalContent3"
+            aria-controls="navbarToggleExternalContent3"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fas fa-bars"></i>
+          </button>
         </div>
-        <div class="collapseBackground shadow-3 p-4">
-            <button class="btn btn-link btn-block border-bottom m-0">
-                <a class="dropdown-item" href="/">Home</a>
-            </button>
-            <button class="btn btn-link btn-block border-bottom m-0">
-                <a class="dropdown-item" href="/pages/current/games">Games</a>
-            </button>
-            <button class="btn btn-link btn-block m-0">
-                <a class="dropdown-item" href="/pages/users/login">Sign In</a>
-            </button>
-        </div>
-    </div>
-    </nav>;
+      </nav>
+      
+    );
 }
--- client/src/components/PageContainer.js
+++ client/src/components/PageContainer.js
@@ -1,12 +1,34 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import NavTabs from "./NavTabs";
-import Homepage from "../pages/Homepage";
-import Games from "../pages/Games";
-import Login from "../pages/LoginPage";
-import SignUp from "../pages/SignUpPage";
+import Homepage from "./pages/Homepage";
+// import Games from "../pages/Games";
+// import Login from "../pages/LoginPage";
+// import SignUp from "../pages/SignUpPage";
 
 export default function PageContainer() {
-    <div id="currentGame" class="currentGame">
-        
+  return (
+    <div>
+      <NavTabs />
+      <div class="collapseBackground shadow-3 p-4">
+        <button class="btn btn-link btn-block border-bottom m-0">
+          <a class="dropdown-item" href="/">
+            Home
+          </a>
+        </button>
+        <button class="btn btn-link btn-block border-bottom m-0">
+          <a class="dropdown-item" href="/games">
+            Games
+          </a>
+        </button>
+        <button class="btn btn-link btn-block m-0">
+          <a class="dropdown-item" href="/login">
+            Login
+          </a>
+        </button>
+      </div>
+      <div id="currentGame" class="currentGame">
+        <Homepage />
+      </div>
     </div>
+  );
 }
