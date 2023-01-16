e53ff833c355f8b7b21d397f5711a621a429925d
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-15 19:39:11 -0700

    cleaning up files

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
+// const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const { authMiddleware } = require("./utils/auth");
+// const { authMiddleware } = require("./utils/auth");
 
-const { typeDefs, resolvers } = require("./schemas");
+// const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -21,22 +21,26 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+// const startApolloServer = async (typeDefs, resolvers) => {
+//   await server.start();
+//   server.applyMiddleware({ app });
 
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port http://localhost:${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
+      // console.log(
+      //   `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      // );
     });
   });
-};
+// };
 
-startApolloServer(typeDefs, resolvers);
+// startApolloServer(typeDefs, resolvers);
