3eeaebabb38613bc5fd15a6f945cc2c37b7ac5f5
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-15 17:50:51 -0700

    added html code to the Homepage and NavTabs files

---
+++ client/src/components/PageContainer.js
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import NavTabs from "./NavTabs";
+import Homepage from "../pages/Homepage";
+import Games from "../pages/Games";
+import Login from "../pages/LoginPage";
+import SignUp from "../pages/SignUpPage";
+
+export default function PageContainer() {
+    <div id="currentGame" class="currentGame">
+        
+    </div>
+}
