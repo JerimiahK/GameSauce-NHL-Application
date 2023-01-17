365e22f7d70e188f35f977ddc37b5ed8e4d0bcc8
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-17 14:23:19 -0700

    added the schema directory code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,5 @@
 const { gql } = require("apollo-server-express");
 
-
 const typeDefs = gql`
   type User {
     _id: ID
@@ -9,4 +8,24 @@ const typeDefs = gql`
     email: String
     finances: [FinancialData]!
   }
+
+  type Logout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    user: [User]!
+  }
+
+  type Mutation {
+    addUser(username: String!, password: String!, email: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
 `;
+
+module.exports = typeDefs;
