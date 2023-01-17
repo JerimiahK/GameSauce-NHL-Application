edb615d949efe1844fbcab20eff21ee17d0cf761
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-17 14:28:46 -0700

    added the apollo client to the front end App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,35 @@
 import PageContainer from "./components/PageContainer";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <PageContainer />
+    <ApolloProvider client={client}>
+      <PageContainer />
+    </ApolloProvider>
   );
 }
 
