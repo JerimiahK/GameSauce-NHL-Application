1d8c32a940633386e94fd1de1393d739f031144f
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-17 13:46:04 -0700

    changed all class to className in the Games page

--- client/src/components/pages/Games.js
+++ client/src/components/pages/Games.js
@@ -1,32 +1,32 @@
 export default function Games() {
     return (
-      <div id="currentGame" class="currentGame">
-        <div id="allGames" class="currentGameBox">
-          <div id="allGamesBox" class="container">
-            <div class="allGamesTeamsBox">
-              <div id="allGamesAway" class="allGamesRow">
+      <div id="currentGame" className="currentGame">
+        <div id="allGames" className="currentGameBox">
+          <div id="allGamesBox" className="container">
+            <div className="allGamesTeamsBox">
+              <div id="allGamesAway" className="allGamesRow">
                 <p>Anaheim Ducks</p>
-                <p class="gameScore">0</p>
+                <p className="gameScore">0</p>
               </div>
-              <div id="allGamesHome" class="allGamesRow">
+              <div id="allGamesHome" className="allGamesRow">
                 <p>Colorado Avalanche</p>
-                <p class="gameScore">2</p>
+                <p className="gameScore">2</p>
               </div>
             </div>
-            <p class="gameStatus">Scheduled</p>
+            <p className="gameStatus">Scheduled</p>
           </div>
-          <div id="allGamesBox" class="container">
-            <div class="allGamesTeamsBox">
-              <div id="allGamesAway" class="allGamesRow">
+          <div id="allGamesBox" className="container">
+            <div className="allGamesTeamsBox">
+              <div id="allGamesAway" className="allGamesRow">
                 <p>Anaheim Ducks</p>
-                <p class="gameScore">0</p>
+                <p className="gameScore">0</p>
               </div>
-              <div id="allGamesHome" class="allGamesRow">
+              <div id="allGamesHome" className="allGamesRow">
                 <p>Colorado Avalanche</p>
-                <p class="gameScore">2</p>
+                <p className="gameScore">2</p>
               </div>
             </div>
-            <p class="gameStatus">Scheduled</p>
+            <p className="gameStatus">Scheduled</p>
           </div>
         </div>
       </div>
