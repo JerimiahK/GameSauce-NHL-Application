a9058828607cc3ec8859145a4678c80ba8b4a52b
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-17 11:36:01 -0700

    fixed the homepage stats column styling

--- client/src/components/pages/Games.js
+++ client/src/components/pages/Games.js
@@ -2,20 +2,32 @@ export default function Games() {
     return (
       <div id="currentGame" class="currentGame">
         <div id="allGames" class="currentGameBox">
-          <h2 id="todayGameText">Today's Games</h2>
-            <div id="allGamesBox" class="container">
-              <div class="allGamesTeamsBox">
-                <div id="allGamesAway" class="allGamesRow">
-                  <p>Anaheim Ducks</p>
-                  <p class="gameScore">0</p>
-                </div>
-                <div id="allGamesHome" class="allGamesRow">
-                  <p>Colorado Avalanche</p>
-                  <p class="gameScore">2</p>
-                </div>
+          <div id="allGamesBox" class="container">
+            <div class="allGamesTeamsBox">
+              <div id="allGamesAway" class="allGamesRow">
+                <p>Anaheim Ducks</p>
+                <p class="gameScore">0</p>
               </div>
-              <p class="gameStatus">Scheduled</p>
+              <div id="allGamesHome" class="allGamesRow">
+                <p>Colorado Avalanche</p>
+                <p class="gameScore">2</p>
+              </div>
             </div>
+            <p class="gameStatus">Scheduled</p>
+          </div>
+          <div id="allGamesBox" class="container">
+            <div class="allGamesTeamsBox">
+              <div id="allGamesAway" class="allGamesRow">
+                <p>Anaheim Ducks</p>
+                <p class="gameScore">0</p>
+              </div>
+              <div id="allGamesHome" class="allGamesRow">
+                <p>Colorado Avalanche</p>
+                <p class="gameScore">2</p>
+              </div>
+            </div>
+            <p class="gameStatus">Scheduled</p>
+          </div>
         </div>
       </div>
     );
--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -2,60 +2,60 @@ export default function Homepage() {
   return (
     <div className="currentGameBox">
       <div className="currentGameHeader">
-          <div className="headerRow">
-            <div className="col-5 headerColumn">
-              <h6 className="headerTeam">Anaheim Ducks</h6>
-              <p className="teamRecord">13-23-5</p>
-              <h1 className="headerScore">0</h1>
-            </div>
-            <div>
-              <p className="headerPeriod">1st Period</p>
-              <p className="headerTime">20:00</p>
-            </div>
-            <div className="col-5 headerColumn">
-              <h6 className="headerTeam">Colorado Avalanche</h6>
-              <p className="teamRecord">21-17-4</p>
-              <h1 className="headerScore">0</h1>
-            </div>
+        <div className="headerRow">
+          <div className="col-5 headerColumn">
+            <h6 className="headerTeam">Anaheim Ducks</h6>
+            <p className="teamRecord">13-23-5</p>
+            <h1 className="headerScore">0</h1>
           </div>
+          <div>
+            <p className="headerPeriod">1st Period</p>
+            <p className="headerTime">20:00</p>
+          </div>
+          <div className="col-5 headerColumn">
+            <h6 className="headerTeam">Colorado Avalanche</h6>
+            <p className="teamRecord">21-17-4</p>
+            <h1 className="headerScore">0</h1>
+          </div>
+        </div>
       </div>
       <div id="statsColumn" className="container text-center">
-        <div className="row">
-          <div className="col-6 columnLeft">
+        <div className="currentRow">
+          <div className="column">
             <p className="staticValue">S.O.G:</p>
             <p className="liveStat">0</p>
           </div>
-          <div className="col-6 columnRight">
+          <div className="column">
             <p className="staticValue">S.O.G:</p>
             <p className="liveStat">0</p>
           </div>
         </div>
-        <div className="row">
-          <div className="col-6 columnLeft">
+        <div className="currentRow">
+          <div className="column">
             <p className="staticValue">Faceoff Win %</p>
             <p className="liveStat">0%</p>
           </div>
-          <div className="col-6 columnRight">
+          <div className="column">
             <p className="staticValue">Faceoff Win %</p>
             <p className="liveStat">0%</p>
           </div>
         </div>
-        <div className="row">
-          <div className="col-6 columnLeft">
+        <div className="currentRow">
+          <div className="column">
             <p className="staticValue">Hits</p>
             <p className="liveStat">0</p>
           </div>
-          <div className="col-6 columnRight">
+          <div className="column">
             <p className="staticValue">Hits</p>
             <p className="liveStat">0</p>
           </div>
         </div>
-        <div className="lastRow row">
-          <div id="homeGoalie" className="col-6 columnLeft">
+        <div className="currentRow">
+          <div className="column">
             <p className="staticValue">PIM</p>
             <p className="liveStat">0</p>
           </div>
-          <div id="awayGoalie" className="col-6 columnRight">
+          <div className="column">
             <p className="staticValue">PIM</p>
             <p className="liveStat">0</p>
           </div>
