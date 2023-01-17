ec9f9c511bab1bd5a6dd5d260fb8b930bf81352f
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-17 13:44:43 -0700

    changed all class to className in React pages

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,33 +1,33 @@
 export default function Login() {
   return (
-    <div class="currentGame">
-      <div id="loginBox" class="currentGameBox">
+    <div className="currentGame">
+      <div id="loginBox" className="currentGameBox">
         <form id="loginForm">
           <h1>LOGIN</h1>
           <div className="inputBox">
-            <label for="userEmail" name="email" class="form-label">
+            <label htmlFor="userEmail" name="email" className="form-label">
               Email Address
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               placeholder="Enter Email"
               id="userLoginEmail"
             />
           </div>
           <div className="inputBox">
-            <label for="userPassword" name="password" class="form-label">
+            <label htmlFor="userPassword" name="password" className="form-label">
               Password
             </label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               placeholder="Enter Password"
               id="userLoginPassword"
             />
           </div>
           <div className="formButtonContainer">
-            <button id="loginButton" type="submit" class="btn formButton">
+            <button id="loginButton" type="submit" className="btn formButton">
               <a className="formButton" href="/">
                 Login
               </a>
--- client/src/components/pages/SignUpPage.js
+++ client/src/components/pages/SignUpPage.js
@@ -1,41 +1,41 @@
 export default function SignUp() {
   return (
-    <div class="currentGame">
-      <div id="loginBox" class="currentGameBox">
+    <div className="currentGame">
+      <div id="loginBox" className="currentGameBox">
         <form id="loginForm">
           <h1>SIGN-UP</h1>
-          <div class="inputBox">
-            <label for="userEmail" name="email" class="form-label">
+          <div className="inputBox">
+            <label htmlFor="userEmail" name="email" className="form-label">
               Email Address
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               placeholder="Enter Email"
               id="userSignUpEmail"
             />
           </div>
-          <div class="inputBox">
-            <label for="userPassword" name="password" class="form-label">
+          <div className="inputBox">
+            <label htmlFor="userPassword" name="password" className="form-label">
               Create A Password
             </label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               placeholder="Enter Password"
               id="userSignUpPassword"
             />
           </div>
           <div className="formButtonContainer">
-            <button id="loginButton" type="submit" class="btn formButton">
+            <button id="loginButton" type="submit" className="btn formButton">
               <a className="formButton" href="/">
-                Login
+                Sign-Up
               </a>
             </button>
           </div>
         </form>
         <p id="loginCreate">
-          Already Have An Account? Click <a href="/pages/users/login">Here</a>{" "}
+          Already Have An Account? Click <a href="/login">Here</a>{" "}
           To Login!
         </p>
       </div>
