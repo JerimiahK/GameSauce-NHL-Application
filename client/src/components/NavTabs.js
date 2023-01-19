f1041a2e8047e23e30193e5a295322cd96a31b34
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-19 16:02:13 -0700

    added conditional rendering for login/logout buttons

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -3,9 +3,15 @@ import Button from "@mui/material/Button";
 import HomeIcon from "@mui/icons-material/Home";
 import SportsHockeyIcon from "@mui/icons-material/SportsHockey";
 import LoginIcon from "@mui/icons-material/Login";
-
+import LogoutIcon from "@mui/icons-material/Logout";
+import Auth from "../utils/auth";
 
 export default function NavTabs() {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <div className="navContainer">
       <div className="headerText">
@@ -13,62 +19,125 @@ export default function NavTabs() {
           Game Sauce
         </a>
       </div>
-      <div className="headerButtons">
-        <Button sx={{ minWidth: "0px" }}>
-          <HomeIcon sx={{ color: "#92b9e0" }} />
-          <Typography
-            sx={{
-              color: "#C5C6C7",
-            }}
-          >
-            <Link
-              sx={{
-                textDecoration: "none",
-              }}
-              color="inherit"
-              href="/"
-            >
-              Home
-            </Link>
-          </Typography>
-        </Button>
-        <Button sx={{ minWidth: "0px" }}>
-          <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
-          <Typography
-            sx={{
-              color: "#C5C6C7",
-            }}
-          >
-            <Link
-              sx={{
-                textDecoration: "none",
-              }}
-              color="inherit"
-              href="/games"
-            >
-              Games
-            </Link>
-          </Typography>
-        </Button>
-        <Button sx={{ minWidth: "0px" }}>
-          <LoginIcon sx={{ color: "#92b9e0" }} />
-          <Typography
-            sx={{
-              color: "#C5C6C7",
-            }}
-          >
-            <Link
-              sx={{
-                textDecoration: "none",
-              }}
-              color="inherit"
-              href="/login"
-            >
-              Login
-            </Link>
-          </Typography>
-        </Button>
-      </div>
+      {Auth.loggedIn() !== true ? (
+        <>
+          <div className="headerButtons">
+            <Button sx={{ minWidth: "0px" }}>
+              <HomeIcon sx={{ color: "#92b9e0" }} />
+              <Typography
+                sx={{
+                  color: "#C5C6C7",
+                }}
+              >
+                <Link
+                  sx={{
+                    textDecoration: "none",
+                  }}
+                  color="inherit"
+                  href="/"
+                >
+                  Home
+                </Link>
+              </Typography>
+            </Button>
+            <Button sx={{ minWidth: "0px" }}>
+              <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
+              <Typography
+                sx={{
+                  color: "#C5C6C7",
+                }}
+              >
+                <Link
+                  sx={{
+                    textDecoration: "none",
+                  }}
+                  color="inherit"
+                  href="/games"
+                >
+                  Games
+                </Link>
+              </Typography>
+            </Button>
+            <Button sx={{ minWidth: "0px" }}>
+              <LogoutIcon sx={{ color: "#92b9e0" }} />
+              <Typography
+                sx={{
+                  color: "#C5C6C7",
+                }}
+              >
+                <Link
+                  sx={{
+                    textDecoration: "none",
+                  }}
+                  color="inherit"
+                  href="/login"
+                >
+                  Logout
+                </Link>
+              </Typography>
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="headerButtons">
+            <Button sx={{ minWidth: "0px" }}>
+              <HomeIcon sx={{ color: "#92b9e0" }} />
+              <Typography
+                sx={{
+                  color: "#C5C6C7",
+                }}
+              >
+                <Link
+                  sx={{
+                    textDecoration: "none",
+                  }}
+                  color="inherit"
+                  href="/"
+                >
+                  Home
+                </Link>
+              </Typography>
+            </Button>
+            <Button sx={{ minWidth: "0px" }}>
+              <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
+              <Typography
+                sx={{
+                  color: "#C5C6C7",
+                }}
+              >
+                <Link
+                  sx={{
+                    textDecoration: "none",
+                  }}
+                  color="inherit"
+                  href="/games"
+                >
+                  Games
+                </Link>
+              </Typography>
+            </Button>
+            <Button sx={{ minWidth: "0px" }}>
+              <LoginIcon sx={{ color: "#92b9e0" }} />
+              <Typography
+                sx={{
+                  color: "#C5C6C7",
+                }}
+              >
+                <Link
+                  sx={{
+                    textDecoration: "none",
+                  }}
+                  color="inherit"
+                  href="/login"
+                >
+                  Login
+                </Link>
+              </Typography>
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
