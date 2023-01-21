0d11d400f23494962c272d88857eb32288f81b07
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-20 20:51:37 -0700

    adding game card to the games container

--- client/src/components/pages/GamesComponents/Games.js
+++ client/src/components/pages/GamesComponents/Games.js
@@ -1,33 +1,40 @@
+import React, { useEffect, useState } from "react";
+import GamesCard from "./GamesCard";
 
 export default function Games() {
-    return (
-        <div id="allGames" className="currentGameBox">
-          <div id="allGamesBox" className="container">
-            <div className="allGamesTeamsBox">
-              <div id="allGamesAway" className="allGamesRow">
-                <p>Anaheim Ducks</p>
-                <p className="gameScore">0</p>
-              </div>
-              <div id="allGamesHome" className="allGamesRow">
-                <p>Colorado Avalanche</p>
-                <p className="gameScore">2</p>
-              </div>
-            </div>
-            <p className="gameStatus">Scheduled</p>
-          </div>
-          <div id="allGamesBox" className="container">
-            <div className="allGamesTeamsBox">
-              <div id="allGamesAway" className="allGamesRow">
-                <p>Anaheim Ducks</p>
-                <p className="gameScore">0</p>
-              </div>
-              <div id="allGamesHome" className="allGamesRow">
-                <p>Colorado Avalanche</p>
-                <p className="gameScore">2</p>
-              </div>
-            </div>
-            <p className="gameStatus">Scheduled</p>
-          </div>
-        </div>
-    );
+  const url = `http://statsapi.web.nhl.com/api/v1/schedule`;
+
+  async function getData() {
+    const currentData = await fetch(url, {
+      method: "GET",
+    }).then((res) => res.json());
+
+    const allGames = currentData.dates[0].games;
+
+    let gamesArray = [];
+
+    for (let g of allGames) {
+      gamesArray.push({
+        id: g.gamePk,
+        homeName: g.teams.home.team.name,
+        homeScore: g.teams.home.score,
+        awayName: g.teams.away.team.name,
+        awayScore: g.teams.away.score,
+        status: g.status.detailedState,
+      });
+    }
+
+    const todaysGames = gamesArray
+    console.log(todaysGames);
+  }
+
+   useEffect(() => {
+     getData();
+   }, []);
+
+  return (
+    <div id="allGames" className="currentGameBox">
+      {}
+    </div>
+  );
 }
--- client/src/components/pages/GamesComponents/GamesCard.js
+++ client/src/components/pages/GamesComponents/GamesCard.js
@@ -1,18 +1,111 @@
-export default function GamesCard(){
+import React, { useEffect, useState } from "react";
 
-    return (
-        <div id="allGamesBox" className="container">
-            <div className="allGamesTeamsBox">
-              <div id="allGamesAway" className="allGamesRow">
-                <p>Anaheim Ducks</p>
-                <p className="gameScore">0</p>
-              </div>
-              <div id="allGamesHome" className="allGamesRow">
-                <p>Colorado Avalanche</p>
-                <p className="gameScore">2</p>
-              </div>
-            </div>
-            <p className="gameStatus">Scheduled</p>
-          </div>
-    )
+export default function GamesCard() {
+  const url = `http://statsapi.web.nhl.com/api/v1/schedule`;
+
+  const [awayTeam, setAwayTeam] = useState();
+  const [awayScore, setAwayScore] = useState();
+  const [homeTeam, setHomeTeam] = useState();
+  const [homeScore, setHomeScore] = useState();
+
+  async function getData() {
+    const currentData = await fetch(url, {
+      method: "GET",
+    }).then((res) => res.json());
+
+    const allGames = currentData.dates[0].games;
+
+    let gamesArray = [];
+
+    for (let g of allGames) {
+      gamesArray.push({
+        id: g.gamePk,
+        homeName: g.teams.home.team.name,
+        homeScore: g.teams.home.score,
+        awayName: g.teams.away.team.name,
+        awayScore: g.teams.away.score,
+        status: g.status.detailedState,
+      });
+    }
+
+    const boxAwayTeam = gamesArray.awayName
+    setAwayTeam(boxAwayTeam)
+
+    const boxAwayScore = gamesArray.awayScore
+    setAwayScore(boxAwayScore);
+
+    const boxHomeTeam = gamesArray.homeName
+    setHomeTeam(boxHomeTeam);
+
+    const boxHomeScore = gamesArray.homeScore
+    setHomeScore(boxHomeScore);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // router.get("/game/:id", async (req, res) => {
+  //   let currentTeamRecords;
+  //   let teamRecords = [];
+  //   let gameID;
+
+  //     const todaysGames = await fetch(url, {
+  //       method: "GET",
+  //     });
+
+  //     const currentData = await todaysGames.json();
+
+  //     const allGames = currentData.dates[0].games;
+
+  //     for (let g of allGames) {
+  //       teamRecords.push({
+  //         id: g.gamePk,
+  //         homeWins: g.teams.home.leagueRecord.wins,
+  //         homeLosses: g.teams.home.leagueRecord.losses,
+  //         homeTies: g.teams.home.leagueRecord.ot,
+  //         awayWins: g.teams.away.leagueRecord.wins,
+  //         awayLosses: g.teams.away.leagueRecord.losses,
+  //         awayTies: g.teams.away.leagueRecord.ot,
+  //       });
+  //     }
+
+  //     gameID = req.params.id;
+
+  //     for (let r of teamRecords) {
+  //       if (gameID == r.id) {
+  //         currentTeamRecords = {
+  //           homeWins: r.homeWins,
+  //           homeLosses: r.homeLosses,
+  //           homeTies: r.homeTies,
+  //           awayWins: r.awayWins,
+  //           awayLosses: r.awayLosses,
+  //           awayTies: r.awayTies,
+  //         };
+  //       }
+  //     }
+
+  //     const box = `https://statsapi.web.nhl.com/api/v1/game/${gameID}/feed/live`;
+
+  //     const gameFetch = await fetch(box, {
+  //       method: "GET",
+  //     });
+
+  //     const liveData = await gameFetch.json();
+
+  return (
+    <div id="allGamesBox" className="container">
+      <div className="allGamesTeamsBox">
+        <div id="allGamesAway" className="allGamesRow">
+          <p>{awayTeam}</p>
+          <p className="gameScore">{awayScore}</p>
+        </div>
+        <div id="allGamesHome" className="allGamesRow">
+          <p>{homeTeam}</p>
+          <p className="gameScore">{homeScore}</p>
+        </div>
+      </div>
+      <p className="gameStatus">Scheduled</p>
+    </div>
+  );
 }
