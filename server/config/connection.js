9fc54e8b74b78889f6ec5062fd53404deeec593a
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-23 10:10:57 -0700

    fixing deployment issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/gameSauceDB",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/gameSauceDB",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
