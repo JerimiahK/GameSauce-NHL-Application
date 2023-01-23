383f6da27291474d89014568ba4f50bf00431056
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-23 10:23:44 -0700

    changed bcrypt require in to bcryptjs

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const favoriteTeams = require("./FavoriteTeams");
 
 const userSchema = new Schema({
