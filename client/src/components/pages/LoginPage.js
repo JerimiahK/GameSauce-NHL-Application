fdf63d346ecd018ccf9fcda4b9fa75d332cd8fd9
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-14 12:43:50 -0600

    restyled the login page to be more modern and dynamic

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 import { LOGIN } from "../../utils/mutations";
-import { Container, TextField, Button, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { TextField, Button } from "@mui/material";
 import styled from "@emotion/styled";
 import Auth from "../../utils/auth";
 
@@ -46,61 +45,79 @@ export default function Login() {
   };
 
   return (
-    <>
-      <Container style={style.form}>
-        <Box component="form" onSubmit={handleFormSubmit}>
-          <StyledTextField
-            placeholder="Your email"
-            name="email"
-            type="email"
-            value={formState.email}
-            onChange={handleChange}
-            label="email"
-            variant="standard"
-          />
-          <StyledTextField
-            placeholder="password"
-            name="password"
-            type="password"
-            value={formState.password}
-            onChange={handleChange}
-            label="password"
-            variant="standard"
-          />
-          {error ? (
-            <div>
-              <p className="error-text">
-                The provided credentials are incorrect
-              </p>
-            </div>
-          ) : null}
-          <Box textAlign="center">
-            <Button
-              type="submit"
-              sx={{
-                mt: 5,
-                border: 1,
-                backgroundColor: "#92b9e0",
-                color: "#C5C6C7",
-              }}
-            >
-              Login
-            </Button>
-          </Box>
-        </Box>
-        <Typography id="signupCreate">
+    <div style={style.container}>
+      <form onSubmit={handleFormSubmit} style={style.form}>
+        <StyledTextField
+          style={style.input}
+          placeholder="Your Email"
+          name="email"
+          type="email"
+          value={formState.email}
+          onChange={handleChange}
+          label="Email"
+          variant="standard"
+        />
+        <StyledTextField
+          style={style.input}
+          placeholder="Password"
+          name="password"
+          type="password"
+          value={formState.password}
+          onChange={handleChange}
+          label="Password"
+          variant="standard"
+        />
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <Button
+          type="submit"
+          sx={{
+            mt: 5,
+            border: 1,
+            backgroundColor: "#92b9e0",
+            color: "#fff",
+          }}
+        >
+          Login
+        </Button>
+        <p style={style.text}>
           Don't Have An Account? Click <a href="/signup">Here</a> To Create An
           Account!
-        </Typography>
-      </Container>
-    </>
+        </p>
+      </form>
+    </div>
   );
 }
 
 const style = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
   form: {
     margin: 10,
-    backgroundColor: "black",
-    height: "100%",
-  }
-}
+    height: "90vh",
+    width: "90vw",
+    backgroundColor: "#282828",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "60%",
+    backgroundColor: "#fff",
+    border: "#92b9e0 5px solid",
+  },
+  text: {
+    position: "absolute",
+    bottom: 0,
+    color: "#fff",
+    textAlign: "center",
+    padding: 15,
+  },
+};
