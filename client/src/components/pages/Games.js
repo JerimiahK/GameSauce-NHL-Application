de64ff8611d98c9da31c6aec63a30d5f8c1c6bb9
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-23 19:00:27 -0600

    Cleaned up the styling on the homepage, made it mobile responsive.

--- client/src/components/pages/Games.js
+++ client/src/components/pages/Games.js
@@ -1,72 +1,78 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 export default function Games() {
-  const url = `https://statsapi.web.nhl.com/api/v1/schedule`;
-  const [games, setTodaysGames] = useState();
+    const url = `https://statsapi.web.nhl.com/api/v1/schedule`;
+    const [games, setTodaysGames] = useState();
 
-  useEffect(() => {
-    async function getData() {
-      const currentData = await fetch(url, {
-        method: "GET",
-      }).then((res) => res.json());
+    useEffect(() => {
+        async function getData() {
+            const currentData = await fetch(url, {
+                method: "GET",
+            }).then((res) => res.json());
 
-      const allGames = currentData.dates[0].games;
+            const allGames = currentData.dates[0].games;
 
-      let gamesArray = [];
+            let gamesArray = [];
 
-      for (let g of allGames) {
-        gamesArray.push({
-          id: g.gamePk,
-          homeName: g.teams.home.team.name,
-          homeScore: g.teams.home.score,
-          awayName: g.teams.away.team.name,
-          awayScore: g.teams.away.score,
-          status: g.status.detailedState,
-        });
-      }
+            for (let g of allGames) {
+                gamesArray.push({
+                    id: g.gamePk,
+                    homeName: g.teams.home.team.name,
+                    homeScore: g.teams.home.score,
+                    awayName: g.teams.away.team.name,
+                    awayScore: g.teams.away.score,
+                    status: g.status.detailedState,
+                });
+            }
 
-      const todaysGames = gamesArray;
-      setTodaysGames(todaysGames);
-      console.log(allGames[0].link);
-    }
-    getData();
-  }, [url]);
+            const todaysGames = gamesArray;
+            setTodaysGames(todaysGames);
+            console.log(allGames[0].link);
+        }
+        getData();
+    }, [url]);
 
-  
-
-  return (
-      <>
-          {games?.map((game) => (
-              <div className="gameBoxContainer">
-                  <Link className="allGamesLink" to={`/game/${game.id}`}>
-                      {game.status === "Final" ? (
-                          <div id="allGamesBoxFinal" className="container">
-                              <div id="allGamesAway" className="allGamesRow">
-                                  <p>{game.awayName}</p>
-                                  <p className="gameScore">{game.awayScore}</p>
-                              </div>
-                              <div id="allGamesHome" className="allGamesRow">
-                                  <p>{game.homeName}</p>
-                                  <p className="gameScore">{game.homeScore}</p>
-                              </div>
-                              <h3>Final</h3>
-                          </div>
-                      ) : (
-                          <div id="allGamesBox" className="container">
-                              <div id="allGamesAway" className="allGamesRow">
-                                  <p>{game.awayName}</p>
-                                  <p className="gameScore">{game.awayScore}</p>
-                              </div>
-                              <div id="allGamesHome" className="allGamesRow">
-                                  <p>{game.homeName}</p>
-                                  <p className="gameScore">{game.homeScore}</p>
-                              </div>
-                          </div>
-                      )}
-                  </Link>
-              </div>
-          ))}
-      </>
-  );
+    return (
+        <>
+            {games?.map((game) => (
+                <div className="gameBoxContainer">
+                    <Link className="allGamesLink" to={`/game/${game.id}`}>
+                        {game.status === "Final" ? (
+                            <div id="allGamesBoxFinal" className="container">
+                                <div id="allGamesAway" className="allGamesRow">
+                                    <p>{game.awayName}</p>
+                                    <p className="gameScore">
+                                        {game.awayScore}
+                                    </p>
+                                </div>
+                                <div id="allGamesHome" className="allGamesRow">
+                                    <p>{game.homeName}</p>
+                                    <p className="gameScore">
+                                        {game.homeScore}
+                                    </p>
+                                </div>
+                                <h5>Final</h5>
+                            </div>
+                        ) : (
+                            <div id="allGamesBox" className="container">
+                                <div id="allGamesAway" className="allGamesRow">
+                                    <p>{game.awayName}</p>
+                                    <p className="gameScore">
+                                        {game.awayScore}
+                                    </p>
+                                </div>
+                                <div id="allGamesHome" className="allGamesRow">
+                                    <p>{game.homeName}</p>
+                                    <p className="gameScore">
+                                        {game.homeScore}
+                                    </p>
+                                </div>
+                            </div>
+                        )}
+                    </Link>
+                </div>
+            ))}
+        </>
+    );
 }
